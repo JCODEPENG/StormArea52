8bb058594434f77edb66474ef89c67fbef83bf1e
Thomas Nakagawa <thomasnakagawa@gmail.com>
2019-10-30 13:59:18 -0700

    Initial commit
    Added basic file stucture, basic character movement, readme

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls a playable character using a Rigidbody
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterController : MonoBehaviour
+{
+    [Header("Controls")]
+    [SerializeField] private KeyCode MoveUpKey = KeyCode.W;
+    [SerializeField] private KeyCode MoveLeftKey = KeyCode.A;
+    [SerializeField] private KeyCode MoveDownKey = KeyCode.S;
+    [SerializeField] private KeyCode MoveRightKey = KeyCode.D;
+
+    [Header("Movement")]
+    [SerializeField] private float MovementForce = 6000f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get reference to Rigidbody so we can apply forces to it to move the character around
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = GetMovementDirectionFromPlayerInputs();
+        if (movementDirection != Vector3.zero)
+        {
+            rb.AddForce(movementDirection.normalized * MovementForce * Time.deltaTime);
+        }
+    }
+
+    private Vector3 GetMovementDirectionFromPlayerInputs()
+    {
+        Vector3 movementDirection = Vector3.zero;
+
+        // read keyboard inputs to determine which direction to move in.
+        if (Input.GetKey(MoveUpKey))
+        {
+            movementDirection += transform.forward;
+        }
+        if (Input.GetKey(MoveLeftKey))
+        {
+            movementDirection -= transform.right;
+        }
+        if (Input.GetKey(MoveDownKey))
+        {
+            movementDirection -= transform.forward;
+        }
+        if (Input.GetKey(MoveRightKey))
+        {
+            movementDirection += transform.right;
+        }
+
+        return movementDirection;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+﻿# Storm Area 52
+SFU Surge project
+
+## About
+Storm Area 52 is a multiplayer game made in Unity about working together to retreive as much evidence before time runs out.
+
+## Dev setup
+Install Unity, clone the repo and open the project.
+
+The project uses ProBuilder and ProGrids, so you may need to install them from the package manager.
+To do this:
+* open Unity and click Window -> Package Manager to open the package manager
+* search for "ProBuilder" and install it
+* make sure "Show preview packages" is enabled by clicking Advanced -> Show preview packages
+* search for "ProGrids" and install it
