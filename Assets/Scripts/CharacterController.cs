f80d977e77bbc07edf19bfce101f47d5b74ac331
Rustem-42 <rustem2001@gmail.com>
2019-11-08 14:05:51 -0800

    Add implementation of collectable pick up and throw

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Controls a playable character using a Rigidbody
@@ -13,13 +14,16 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private KeyCode MoveLeftKey = KeyCode.A;
     [SerializeField] private KeyCode MoveDownKey = KeyCode.S;
     [SerializeField] private KeyCode MoveRightKey = KeyCode.D;
+    [SerializeField] private KeyCode Throw = KeyCode.Space;
 
     [Header("Movement")]
     [SerializeField] private float MovementForce = 100f;
-
+    public GameObject Capsule;
+    public Text countText;
     private Rigidbody rb;
     private Vector3 CurrentMovementDirection = Vector3.zero;
-
+    private int count;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,18 +33,35 @@ public class CharacterController : MonoBehaviour
         {
             throw new MissingComponentException("Character controller needs a rigidbody");
         }
+        count = 0;
+        SetCountText();
+
+       
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         CurrentMovementDirection = GetMovementDirectionFromPlayerInputs();
+
+        if (count > 0 & Input.GetKey(Throw))
+        {
+            count--;
+            SetCountText();
+            Instantiate(Capsule, new Vector3(rb.position.x, rb.position.y, rb.position.z+1), Quaternion.identity);
+            
+            //Instantiate(Capsule, rb.position.x), Quaternion.identity);
+        }
     }
 
     private Vector3 GetMovementDirectionFromPlayerInputs()
     {
         Vector3 movementDirection = Vector3.zero;
 
+
+       
         // read keyboard inputs to determine which direction to move in.
         if (Input.GetKey(MoveUpKey))
         {
@@ -69,5 +90,20 @@ public class CharacterController : MonoBehaviour
         {
             rb.AddForce(CurrentMovementDirection.normalized * MovementForce);
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Capsule"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
     }
 }
