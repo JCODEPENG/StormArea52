91710c2ab79fb7efc18a48e57ee78b6fe251191e
katelina7 <jocelyngau@yahoo.com>
2019-11-12 20:49:26 -0800

    Changed some code with given suggestions

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System;
-//not finished, need a visualization for the timer (ex. text)
+
 // countdown timer for the stage
 // once the timer reaches 0.0 sec activate fail state
 
 public class CountdownTimer : MonoBehaviour
 {
-    [SerializeField] private float GameTime;
-    [SerializeField] public Text num ;
+    [SerializeField] private float GameTime = 0.0f;
+    public Text num ;
 
     private float timer;
     private string twodig;
@@ -20,19 +18,15 @@ public class CountdownTimer : MonoBehaviour
     {
         timer = GameTime; 
         twodig = String.Format("{0:0.00}", timer);
-        num.GetComponent<Text>().text = twodig;
-        
-        
+        num.text = twodig;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (timer >= 0.0f && FailState == false)
-        //if (timer >= 0.0f)
         {
             timer -= Time.deltaTime;
-            
         }
         else
         {
@@ -40,6 +34,6 @@ public class CountdownTimer : MonoBehaviour
             FailState = true;
         }
         twodig = String.Format("{0:0.00}", timer);
-        num.GetComponent<Text>().text = twodig;
+        num.text = twodig;
     }
 }
