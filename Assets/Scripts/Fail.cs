8222d7dc0cad063bdfef4276ddfe5afb6cd9c58c
Jocelyn <jocelyngau7@gmail.com>
2019-11-23 21:18:34 -0800

    save progress

---
+++ Assets/Scripts/Fail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStateManager.Instance.RegisterOnStateChange(GameStateManager.GameStates.GAME_OVER_LOSE, load_scene);
+    }
+
+    public void load_scene(){
+        load = GameObject FailScreen;
+        load.SetActive(true);
+    }
+}
