749b8644a246e485b2b06f235520ef9c1844cfab
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2019-11-28 01:43:26 -0800

    TurretRoom
    I designed a little side level that implements turrets haha check it out

---
+++ Assets/Scripts/TurretFunctions/BulletMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public Transform pos;
+    Vector3 force;
+    
+    //float step = 1.0f;
+    float thrust = 1000.0f;
+    private void Update()
+    {
+        //moves bullet forward
+        transform.position += transform.forward * Time.deltaTime * 30f;
+        force = transform.forward;
+        //transform.position = Vector3.MoveTowards(transform.position, pos.position, step * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if bullet hits the player it will cause the player to thrust forward
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody playercomponent = other.GetComponent<Rigidbody>();
+            if (playercomponent == null)
+            {
+                Debug.Log("no rigid body");
+                return;
+            }
+            else
+            {
+                playercomponent.AddForce(force * thrust);
+                Destroy(gameObject);
+            }
+
+        }
+
+        Destroy(gameObject);
+
+    }
+    public void SetTracker(GameObject go)
+    {
+        pos = go.transform;
+    }
+
+}
---
+++ Assets/Scripts/TurretFunctions/TurretShootBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretShootBase : MonoBehaviour
+{
+
+    public Transform muzzle;
+    public GameObject bullet;
+
+
+
+    public virtual void Shoot(GameObject go)
+    {
+        //instantiates a bullet
+        GameObject missilego = Instantiate(bullet, muzzle.transform.position, muzzle.rotation);
+        missilego.GetComponent<BulletMovement>().SetTracker(go);
+
+        //missilego.transform.position += transform.forward * Time.deltaTime;
+       
+    }
+    
+
+}
