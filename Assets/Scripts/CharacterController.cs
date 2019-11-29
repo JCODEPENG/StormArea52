54f3b675832315ff508dfb91375a35e2675c425e
Rustem-42 <57470483+Rustem-42@users.noreply.github.com>
2019-11-28 23:01:17 -0800

    New version for collectibles mechanic (#21)
    * New version for collectables mechanic
    
    * Update BigLevel.unity
    
    * collectables_v.2
    
    merged version

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Controls a playable character using a Rigidbody
@@ -13,13 +14,18 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private KeyCode MoveLeftKey = KeyCode.A;
     [SerializeField] private KeyCode MoveDownKey = KeyCode.S;
     [SerializeField] private KeyCode MoveRightKey = KeyCode.D;
+    [SerializeField] private KeyCode ThrowKey = KeyCode.Space;
     [SerializeField] public KeyCode ActionKey = KeyCode.E;
+
     [Header("Movement")]
     [SerializeField] private float MovementForce = 100f;
 
     private Rigidbody rb;
     private Vector3 CurrentMovementDirection = Vector3.zero;
-
+    private Vector3 col_pos;
+    int score = 0;
+    public GameObject collectable;
+    public Text scoretext;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +42,13 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         CurrentMovementDirection = GetMovementDirectionFromPlayerInputs();
+        if (Input.GetKeyDown(ThrowKey) && score>0)
+        {
+            Instantiate(collectable,new Vector3(col_pos.x,col_pos.y,col_pos.z), Quaternion.identity);
+            score--;
+            //scoretext.text = "Player score: " + score.ToString();
+            Debug.Log(score);
+        }
     }
 
     private Vector3 GetMovementDirectionFromPlayerInputs()
@@ -43,27 +56,36 @@ public class CharacterController : MonoBehaviour
         Vector3 movementDirection = Vector3.zero;
 
         // read keyboard inputs to determine which direction to move in.
+
         if (Input.GetKey(MoveUpKey))
         {
             movementDirection += transform.forward;
+            col_pos.Set(rb.position.x, rb.position.y, rb.position.z+2); //for collectable's position
         }
         if (Input.GetKey(MoveLeftKey))
         {
             movementDirection -= transform.right;
+            col_pos.Set(rb.position.x - 2, rb.position.y , rb.position.z);
+
         }
         if (Input.GetKey(MoveDownKey))
         {
             movementDirection -= transform.forward;
+            col_pos.Set(rb.position.x, rb.position.y , rb.position.z-2);
+
         }
         if (Input.GetKey(MoveRightKey))
         {
             movementDirection += transform.right;
+            col_pos.Set(rb.position.x + 2, rb.position.y , rb.position.z);
+
         }
 
         return movementDirection;
+
+
     }
 
-    // Physics updates on FixedUpdate, so apply movement forces here instead of in Update()
     private void FixedUpdate()
     {
         if (CurrentMovementDirection != Vector3.zero)
@@ -76,4 +98,18 @@ public class CharacterController : MonoBehaviour
     }
 
     public float CurrentMovementSpeed => rb.velocity.magnitude;
-}
+
+     void OnCollisionEnter (Collision coll)
+    {
+        if (coll.gameObject.CompareTag("col")) 
+        {
+            score++;
+           // scoretext.text = "Player score: " + score.ToString();
+            Destroy(coll.gameObject);
+            Debug.Log(score);
+
+        }
+    }
+   
+    
+    }
