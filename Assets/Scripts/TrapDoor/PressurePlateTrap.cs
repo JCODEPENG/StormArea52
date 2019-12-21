6d6481462681eded0ea3c5ee51fe628dc7d2212f
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2019-12-20 16:03:11 -0800

    TrapDoor
    Not completed yet but just saving what I have so far on github

---
+++ Assets/Scripts/TrapDoor/PressurePlateTrap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateTrap : MonoBehaviour
+{
+    float turnspeed = 40f;
+    //public GameObject field; //any object which will be activated/deactivated
+    int ch = 0; //first check condition(for material)
+    int obj = 0; //amount of objects on a plate
+    private Color old;
+    public Rotate Rotatefunction;
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        if (ch == 0)
+        {
+            old = render.material.color;
+            ch = 1;
+        }
+       Rotatefunction.ActivateTrapdoor();
+      // field.transform.Rotate(0, 0, 90);
+        render.material.color = Color.green;
+        obj++;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        obj--;
+        if (obj == 0)
+        {
+            render.material.color = old;
+            //field.transform.Rotate(0, 0, 90);
+        }
+    }
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TrapDoor/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float timetomove = 1f;
+
+
+
+    public void ActivateTrapdoor()
+    {
+        StartCoroutine(RotateaBit());
+    }
+
+    private IEnumerator RotateaBit()
+    {
+        Vector3 oldpos = transform.eulerAngles;
+        Vector3 newpos = oldpos + (Vector3.forward * 90);
+
+        float start_time = 0f;
+        while (start_time < timetomove)
+        {
+            transform.eulerAngles = Vector3.Lerp(oldpos, newpos, start_time / timetomove);
+            start_time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+}
